7cf5b252a14b7feb49c2d71472bd252817931f78
Richard Sweeney <richardsweeney@Richards-MacBook-Pro.local>
2012-11-02 15:53:43 +0100

    Fixed navigation anchors from landing page

--- wp-content/themes/xbrdr/page-hem.php
+++ wp-content/themes/xbrdr/page-hem.php
@@ -42,24 +42,24 @@
         <div class="chapter2-icons-container">
 
           <ul class="chapter2-icons icons gom group-1">
-              <li class="i-1 unique"><a href="<?php echo URL; ?>/produkter/xp1/#one"><span>Slitstark</span></a></li>
-              <li class="i-2 unique"><a href="<?php echo URL; ?>/produkter/xp1/#two"><span>L&auml;tt</span></a></li>
-              <li class="i-3 unique"><a href="<?php echo URL; ?>/produkter/xp1/#three"><span>Anpassningsbar</span></a></li>
-              <li class="i-4 unique"><a href="<?php echo URL; ?>/produkter/xp1/#sixteen"><span>Brands&auml;ker</span></a></li>
+              <li class="i-1 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-1"><span>Slitstark</span></a></li>
+              <li class="i-2 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-2"><span>L&auml;tt</span></a></li>
+              <li class="i-3 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-3"><span>Anpassningsbar</span></a></li>
+              <li class="i-4 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-16"><span>Brands&auml;ker</span></a></li>
           </ul>
           <ul class="chapter2-icons icons gom group-2">
-              <li class="i-5 unique"><a href="<?php echo URL; ?>/produkter/xp1/#four"><span>V&auml;derbest&auml;ndig</span></a></li>
-              <li class="i-6 unique"><a href="<?php echo URL; ?>/produkter/xp1/#five"><span>Temperatur-<br />oberoende</span></a></li>
-              <li class="i-7 unique"><a href="<?php echo URL; ?>/produkter/xp1/#eleven"><span>Resistent mot olja<br />och kemikalier</span></a></li>
+              <li class="i-5 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-4"><span>V&auml;derbest&auml;ndig</span></a></li>
+              <li class="i-6 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-5"><span>Temperatur-<br />oberoende</span></a></li>
+              <li class="i-7 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-11"><span>Resistent mot olja<br />och kemikalier</span></a></li>
           </ul>
           <ul class="chapter2-icons icons gom group-3">
-              <li class="i-8 unique"><a href="<?php echo URL; ?>/produkter/xp1/#six"><span>Ergonomisk</span></a></li>
-              <li class="i-9 unique"><a href="<?php echo URL; ?>/produkter/xp1/#eight"><span>Insektsfri</span></a></li>
+              <li class="i-8 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-6"><span>Ergonomisk</span></a></li>
+              <li class="i-9 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-8"><span>Insektsfri</span></a></li>
           </ul>
           <ul class="chapter2-icons icons gom group-4">
-              <li class="i-10 unique"><a href="<?php echo URL; ?>/produkter/xp1/#nine"><span>Hygienisk</span></a></li>
-              <li class="i-11 unique"><a href="<?php echo URL; ?>/produkter/xp1/#ten"><span>Absorberar inte<br />olja och kemikalier</span></a></li>
-              <li class="i-12 unique"><a href="<?php echo URL; ?>/produkter/xp1/#twelve"><span>100 %<br />&aring;tervinningsbar</span></a></li>
+              <li class="i-10 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-9"><span>Hygienisk</span></a></li>
+              <li class="i-11 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-10"><span>Absorberar inte<br />olja och kemikalier</span></a></li>
+              <li class="i-12 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-12"><span>100 %<br />&aring;tervinningsbar</span></a></li>
           </ul>
 
         </div>
@@ -87,19 +87,19 @@
         <div class="chapter3-icons-container">
 
           <ul class="chapter3-icons icons gom group-1">
-            <li class="i-13 unique"><a href="<?php echo URL; ?>/produkter/xp1/#thirteen"><span>L&aring;g<br />livscykelkostnad</span></a></li>
-            <li class="i-14 unique"><a href="<?php echo URL; ?>/produkter/xp1/#fourteen"><span>Utbytbara detaljer</span></a></li>
+            <li class="i-13 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-13"><span>L&aring;g<br />livscykelkostnad</span></a></li>
+            <li class="i-14 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-14"><span>Utbytbara detaljer</span></a></li>
           </ul>
           <ul class="chapter3-icons icons gom group-2">
-            <li class="i-15"><a href="<?php echo URL; ?>/produkter/xp1/#one"><span>Slitstark</span></a></li>
-            <li class="i-16 unique"><a href="<?php echo URL; ?>/produkter/xp1/#fifteen"><span>Oberoende av<br />m&aring;ttstandard</span></a></li>
+            <li class="i-15"><a href="<?php echo URL; ?>/produkter/xp1/#goto-1"><span>Slitstark</span></a></li>
+            <li class="i-16 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-15"><span>Oberoende av<br />m&aring;ttstandard</span></a></li>
           </ul>
           <ul class="chapter3-icons icons gom group-3">
-            <li class="i-17 unique"><a href="<?php echo URL; ?>/produkter/xp1/#sixteen"><span>Brands&auml;ker</span></a></li>
-            <li class="i-18"><a href="<?php echo URL; ?>/produkter/xp1/#twelve"><span>100 %<br />&aring;tervinningsbar</span></a></li>
+            <li class="i-17 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-16"><span>Brands&auml;ker</span></a></li>
+            <li class="i-18"><a href="<?php echo URL; ?>/produkter/xp1/#goto-12"><span>100 %<br />&aring;tervinningsbar</span></a></li>
           </ul>
           <ul class="chapter3-icons icons gom group-4">
-            <li class="i-19"><a href="<?php echo URL; ?>/produkter/xp1/#six"><span>Ergonomisk</span></a></li>
+            <li class="i-19"><a href="<?php echo URL; ?>/produkter/xp1/#goto-6"><span>Ergonomisk</span></a></li>
           </ul>
 
         </div>
@@ -127,15 +127,15 @@
         <div class="chapter4-icons-container">
 
           <ul class="chapter4-icons icons gom group-1">
-            <li class="i-21 unique"><a href="<?php echo URL; ?>/produkter/xp1/#seventeen"><span>Greppvänlig</span></a></li>
+            <li class="i-21 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-17"><span>Greppvänlig</span></a></li>
           </ul>
           <ul class="chapter4-icons icons gom group-2">
-            <li class="i-22 unique"><a href="<?php echo URL; ?>/produkter/xp1/#eighteen"><span>Dammar ej</span></a></li>
+            <li class="i-22 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-18"><span>Dammar ej</span></a></li>
           </ul>
           <ul class="chapter4-icons icons gom group-3">
-            <li class="i-20 unique"><a href="<?php echo URL; ?>/produkter/xp1/#two"><span>L&auml;tt</span></a></li>
-            <li class="i-23 unique"><a href="<?php echo URL; ?>/produkter/xp1/#nineteen"><span>Stick- och<br />flisfri</span></a></li>
-            <li class="i-24 unique"><a href="<?php echo URL; ?>/produkter/xp1/#twenty"><span>Ingen risk för<br />utstickande spikar</span></a></li>
+            <li class="i-20 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-2"><span>L&auml;tt</span></a></li>
+            <li class="i-23 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-19"><span>Stick- och<br />flisfri</span></a></li>
+            <li class="i-24 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-20"><span>Ingen risk för<br />utstickande spikar</span></a></li>
           </ul>
 
         </div>
@@ -162,12 +162,12 @@
         <div class="chapter5-icons-container">
 
           <ul class="chapter5-icons icons gom group-1">
-            <li class="i-25 unique"><a href="<?php echo URL; ?>/produkter/xp1/#twentyone"><span>H&aring;llbar</span></a></li>
-            <li class="i-27"><a href="<?php echo URL; ?>/produkter/xp1/#twelve"><span>100 %<br />&aring;tervinningsbar</span></a></li>
-            <li  class="i-28 unique"><a href="<?php echo URL; ?>/produkter/xp1/#twentythree"><span>Rent, v&auml;rdefullt<br />stålskrot</span></a></li>
+            <li class="i-25 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-21"><span>H&aring;llbar</span></a></li>
+            <li class="i-27"><a href="<?php echo URL; ?>/produkter/xp1/#goto-12"><span>100 %<br />&aring;tervinningsbar</span></a></li>
+            <li  class="i-28 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-23"><span>Rent, v&auml;rdefullt<br />stålskrot</span></a></li>
           </ul>
           <ul class="chapter5-icons icons gom group-2">
-            <li class="i-26 unique"><a href="<?php echo URL; ?>/produkter/xp1/#twentytwo"><span>Motst&aring;ndskraftig</span></a></li>
+            <li class="i-26 unique"><a href="<?php echo URL; ?>/produkter/xp1/#goto-22"><span>Motst&aring;ndskraftig</span></a></li>
           </ul>
 
         </div>
