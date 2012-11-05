987a9dcdb24b2635a1c41b596c42fcf96f350205
Richard Sweeney <richardsweeney@Richards-MacBook-Pro.local>
2012-11-05 15:47:05 +0100

    Fixed admin CSS + 404 page

--- wp-content/themes/xbrdr/404.php
+++ wp-content/themes/xbrdr/404.php
@@ -1,19 +1,18 @@
-
 <?php get_header(); ?>
 
-	<div class="left">
-
-		<article>
-
-      <header>
-			  <h1 class="blog-title">404 file not found</h1>
-      </header>
-			<p>Sorry, the file you were looking for couldn't be found.</p>
-
-		</article>
-
-	</div>
-
-<?php get_sidebar(); ?>
+<div class="container-fluid wider-page" id="page">
+  <div class="row-fluid introtext">
+  </div>
+  <div class="row-fluid standard-page-template">
+  <div class="span3">
+    <?php rps_print_main_navigation(true); ?>
+  </div>
+  <div class="span9">
+    <section>
+      <h1><?php _e('404 Sidan hittades inte.', 'xbrdr'); ?></h1>
+      <p><?php _e('Sidan du letade efter kunde inte hittas!', 'xbrdr'); ?></p>
+    </section>
+  </div>
+</div>
 
 <?php get_footer(); ?>
